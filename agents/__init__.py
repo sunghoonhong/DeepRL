d0113e29eaeacc2be1a5139028a22d55847d0c9e
sunghoonhong <ghdtjdgns02@naver.com>
2019-09-23 01:16:01 +0900

    update agent package

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,12 +1,17 @@
 
 from agents.randomly import Randomly
+from agents.ppo import ImageDiscretePPO, ImageContinuousPPO, RecurrentImageContinuousPPO
 
-__all__ = ['randomly']
+
+__all__ = ['randomly', 'ppo']
 
 AGENT_MAP = {
     'randomly': Randomly,
+    'img_rppo_cont': RecurrentImageContinuousPPO,
+    'img_ppo_cont': ImageContinuousPPO,
+    'img_ppo_disc': ImageDiscretePPO
 }
 
-def make(agent_name, env, **kwargs):
+def make(agent_name, **kwargs):
     agent = AGENT_MAP[agent_name]
-    return agent(env, **kwargs)
+    return agent(**kwargs)
--- agents/agent.py
+++ agents/agent.py
@@ -5,6 +5,7 @@ class Agent:
     def __init__(self, env):
         self.env = env
         self.action_space = env.action_space
+        self.action_num = self.action_space.shape[0]
 
     def get_action(self, state):
         raise NotImplementedError
