756910d2b0fd2c527b83542f9e9ceae3b67dc135
sunghoonhong <ghdtjdgns02@naver.com>
2019-09-23 01:17:08 +0900

    add info

--- agents/randomly.py
+++ agents/randomly.py
@@ -14,29 +14,29 @@ class Randomly(Agent):
     def get_action(self):
         return self.action_space.sample()
 
-    def play(self, render, verbose, delay, ep_label=0):
+    def play(self, render=False, verbose=False, delay=0, ep_label=0):
         done = False
         score = 0.
         step = 0
         self.env.reset()
         if render:
             self.env.render()
-        while not done:
-            
+        while not done:            
             time.sleep(delay)
             action = self.get_action()
             if verbose:
                 stamp = '[EP%dT%d]' % (ep_label, step)
                 act_temp = ('{:.3f} ' * len(action)).format(*action)
                 print(stamp, act_temp, end='\r', flush=True)
-            _, rew, done, _ = self.env.step(action)
+            _, rew, done, info = self.env.step(action)
             step += 1
             score += rew
             if render:
                 self.env.render()
         stat = {
             'score': score,
-            'step': step
+            'step': step,
+            'end': info['end']
         }
         return stat
             
